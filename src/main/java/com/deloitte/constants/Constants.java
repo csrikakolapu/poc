81738782b4debf09dcca18785682fb6aafbc617e
Chaitanya Kumar <csrikakolapu@deloitte.com>
2018-02-12 17:42:29 +0530

    verify git hook

--- src/main/java/com/deloitte/constants/Constants.java
+++ src/main/java/com/deloitte/constants/Constants.java
@@ -1,6 +1,6 @@
 package com.deloitte.constants;
 
-public final class Constants {
+public interface Constants {
 	
 	public static final String SERVER_LOCATION_PROPERTY_NAME = "server-location";
 	public static final String FOLDER_PATH_SESSION_VAR = "folderPath";
