9e537ee8069b518ea87801d6befaf76996237c39
Chaitanya Kumar <csrikakolapu@deloitte.com>
2018-02-12 18:17:07 +0530

    ts

--- src/main/java/com/deloitte/constants/Constants.java
+++ src/main/java/com/deloitte/constants/Constants.java
@@ -5,6 +5,4 @@ public interface Constants {
 	public static final String SERVER_LOCATION_PROPERTY_NAME = "server-location";
 	public static final String FOLDER_PATH_SESSION_VAR = "folderPath";
 	
-		
-	
 }
