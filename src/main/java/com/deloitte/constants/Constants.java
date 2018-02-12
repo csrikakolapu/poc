eee2561031a118ce59c75a112f6f08d46a8f94f4
Chaitanya Kumar <csrikakolapu@deloitte.com>
2018-02-12 18:43:26 +0530

    final commit for master branch

--- src/main/java/com/deloitte/constants/Constants.java
+++ src/main/java/com/deloitte/constants/Constants.java
@@ -1,9 +1,6 @@
 package com.deloitte.constants;
 
-public interface Constants {
-	
+public final class Constants {
 	public static final String SERVER_LOCATION_PROPERTY_NAME = "server-location";
 	public static final String FOLDER_PATH_SESSION_VAR = "folderPath";
-	
-	
 }
